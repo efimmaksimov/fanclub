b8ee7e385106638a5030e2aa4fada0b595de41a7
efimmaksimov <63513165+efimmaksimov@users.noreply.github.com>
2020-04-11 22:54:09 +0500

    Update urls.py

--- page/urls.py
+++ page/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('leave_comment', views.leave_comment, name = 'leave_comment'),
-    path('leave_comment1', views.leave_comment1, name = 'leave_comment1'),
     path('comment/', views.AddComment.as_view(), name='add_comment')
 ]
